eeb24f8851173b6624f1712c95895223437c9f90
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-02-07 19:07:36 +0000

    Monero Cryptonight variants, and add one for v7
    This is the first variant of many, with the intent to improve
    Monero's resistance to ASICs and encourage mining decentralization.

--- src/crypto/chacha.h
+++ src/crypto/chacha.h
@@ -69,10 +69,10 @@ namespace crypto {
     chacha20(data, length, key.data(), reinterpret_cast<const uint8_t*>(&iv), cipher);
   }
 
-  inline void generate_chacha_key(const void *data, size_t size, chacha_key& key) {
+  inline void generate_chacha_key(const void *data, size_t size, chacha_key& key, int cn_variant = 0) {
     static_assert(sizeof(chacha_key) <= sizeof(hash), "Size of hash must be at least that of chacha_key");
     tools::scrubbed_arr<char, HASH_SIZE> pwd_hash;
-    crypto::cn_slow_hash(data, size, pwd_hash.data());
+    crypto::cn_slow_hash(data, size, pwd_hash.data(), cn_variant);
     memcpy(&key, pwd_hash.data(), sizeof(key));
   }
 
